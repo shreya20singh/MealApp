ef2cc551704fd92b169795c0a7292b8a2ab27578
Shreya Singh <shreyasingh200297.ss@gmail.com>
2024-01-15 18:30:00 -0500

    Initial Commit

---
+++ MealApp/MealAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealAppApp.swift
+//  MealApp
+//
+//  Created by Shreya Singh on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct MealAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
